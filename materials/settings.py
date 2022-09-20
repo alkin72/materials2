ee0e45c3ba63d0866088a710f1be6a74324849e3
Dmitry <twindima@gmail.com>
2022-09-20 16:55:26 +0200

    deploy python 3.10.7

--- materials/settings.py
+++ materials/settings.py
@@ -148,9 +148,9 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+    STATICFILES_DIRS = [BASE_DIR / 'static']
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = [BASE_DIR / 'static']  #os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
