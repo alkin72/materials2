ea1a39d9ee890410a925a519e49de884f11619e8
Dmitry <twindima@gmail.com>
2022-10-27 19:40:52 +0200

    deploy python 3.10.7

--- static/docs/js/row_higlight.js
+++
@@ -1,8 +0,0 @@
-
-$("#dt-table tr").click(function(){
-    $(this).addClass('selected').siblings().removeClass('selected');    
-    var value_id=$(this).find('td:first').html();
-    $('input[name="submit_number"]').val(value_id);
-
-    // alert(value);   
-  });
